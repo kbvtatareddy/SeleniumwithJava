caddce03b6371c915a998ecc8e350c40868e598a
kbvtatareddy <kbvtatareddy@gmail.com>
2020-05-03 02:34:48 +0530

    Added a new class "CommitTest"

---
+++ AprilMorning6_30AMBatch/src/com/hdfc/loans/homeloans/CommitTest.java
@@ -0,0 +1,13 @@
+package com.hdfc.loans.homeloans;
+
+public class CommitTest 
+{
+
+	public static void main(String[] args) 
+	{
+	
+		System.out.println("This is to Test Commit functionality beteen Eclispe, Git Hub Desktop Client and Git Hub Server");
+
+	}
+
+}
